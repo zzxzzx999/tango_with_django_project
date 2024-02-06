679712f1e7270916912050b60e1444b3e2faac36
Zhixuan Zhu <2784305z@student.gla.ac.uk>
2024-02-07 05:08:59 +0800

    chapter8

--- rango/views.py
+++ rango/views.py
@@ -21,7 +21,9 @@ def index(request):
 
 
 def about(request):
-    return render(request, 'rango/about.html')
+    print(request.method)
+    print(request.user)
+    return render(request, 'rango/about.html', {})
 
 def show_category(request, category_name_slug):
     context_dict = {}
@@ -30,10 +32,12 @@ def show_category(request, category_name_slug):
         pages = Page.objects.filter(category=category)
         context_dict['pages'] = pages
         context_dict['category'] = category
+        context_dict['current_category'] = category
 
     except Category.DoesNotExist:
         context_dict['category'] = None
         context_dict['pages'] = None
+        context_dict['current_category'] = None
 
     return render(request, 'rango/category.html', context=context_dict)
 
